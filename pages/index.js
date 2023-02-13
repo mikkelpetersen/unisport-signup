a739e3718ba2a30bc92138a7d0b6c4b9d84b758b
Mikkel Petersen <mikkel@mikkelpetersen.dk>
2023-02-13 09:25:55 +0100

    Prettified

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
+import Head from 'next/head';
+import { Inter } from '@next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Unisport Membership Signup</title>
-        <meta name="description" content="Become a Unisport Member for free today and get access to limited products and exclusive offers." />
+        <meta
+          name="description"
+          content="Become a Unisport Member for free today and get access to limited products and exclusive offers."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className={inter.className}>
         <h1>Hello World.</h1>
       </main>
     </>
-  )
+  );
 }
